551f4dc4c0fbdf2d09fec7f54571d60a6ee89ad8
Anton Drauzin <drauzinanton@gmail.com>
2019-12-02 21:38:49 +1000

    Переписал вопросы о статье расхода и сумме за них при помощи цикла for и выставил условия проверки введённых данных, дописал проверку бюджета строки с 15 по 40

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+'use strict';
+
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
     budget: money,
@@ -10,16 +12,32 @@ let appData = {
     saving: false
 };
 
-let question1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let question2 = prompt("Во сколько обойдется?", "");
-let question3 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let question4 = prompt("Во сколько обойдется?", "");
+for (let i = 0; i < 2; i++) {
+    let question1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        question2 = prompt("Во сколько обойдется?", "");
 
-appData.expenses[question1] = question2;
-appData.expenses[question3] = question4;
+    if ((typeof (question1)) === 'string' && (typeof (question1)) != null &&
+        (typeof (question2)) != null && question1 != "" && question2 != "" &&
+        question1.length < 50) {
+        console.log("Введённые данные соответствуют");
+        appData.expenses[question1] = question2;
+    } else {
+        console.log("Введённые данные не соответствуют попробуйте ещё раз");
+        i--;
+    }
+};
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
-alert(appData.budget / 30);
-
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний достаток");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Достаток Выше среднего");
+} else {
+    console.log("Произошла ошибка");
+};
 // Ответы на вопросы
 // 1)в JS существует 7 типов данных
 // 2)При помощи console.log() в скобках 
