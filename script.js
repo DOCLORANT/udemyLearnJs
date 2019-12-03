2e2ec1310749b085e3521677e81bfca95a7f9863
Anton Drauzin <drauzinanton@gmail.com>
2019-12-03 19:19:04 +1000

    Перенёс цикл for в начало кода

--- script.js
+++ script.js
@@ -11,6 +11,38 @@ let appData = {
     income: [],
     saving: false
 };
+
+for (let i = 0; i < 2; i++) {
+    let question1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        question2 = +prompt("Во сколько обойдется?", "");
+
+    if ((typeof (question1)) === 'string' && (typeof (question1)) != null &&
+        (typeof (question2)) != null && question1 != "" && question2 != "" &&
+        question1.length < 50) {
+        console.log("Введённые данные соответствуют");
+        appData.expenses[question1] = question2;
+    } else {
+        console.log("Введённые данные не соответствуют попробуйте ещё раз");
+        i--;
+    }
+};
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний достаток");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Достаток Выше среднего");
+} else {
+    console.log("Произошла ошибка");
+};
+
+//===============================================================================
+// Цикл WHILE
+//===============================================================================
+//
 // let i = 0;
 // while (i < 2) {
 //     let question1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
@@ -40,40 +72,15 @@ let appData = {
 // } else {
 //     console.log("Произошла ошибка");
 // };
-let i = 0;
-do {
-    let question1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        question2 = prompt("Во сколько обойдется?", "");
 
-    if ((typeof (question1)) === 'string' && (typeof (question1)) != null &&
-        (typeof (question2)) != null && question1 != "" && question2 != "" &&
-        question1.length < 50) {
-        console.log("Введённые данные соответствуют");
-        appData.expenses[question1] = question2;
-    } else {
-        console.log("Введённые данные не соответствуют попробуйте ещё раз");
-        i--;
-    }
-    i++;
-}
-while (i < 2);
+//=======================================================================================
+// Цикл DO
+//=======================================================================================
 
-appData.moneyPerDay = appData.budget / 30;
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний достаток");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Достаток Выше среднего");
-} else {
-    console.log("Произошла ошибка");
-};
-
-// for (let i = 0; i < 2; i++) {
+// let i = 0;
+// do {
 //     let question1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         question2 = +prompt("Во сколько обойдется?", "");
+//         question2 = prompt("Во сколько обойдется?", "");
 
 //     if ((typeof (question1)) === 'string' && (typeof (question1)) != null &&
 //         (typeof (question2)) != null && question1 != "" && question2 != "" &&
@@ -84,7 +91,10 @@ if (appData.moneyPerDay < 100) {
 //         console.log("Введённые данные не соответствуют попробуйте ещё раз");
 //         i--;
 //     }
-// };
+//     i++;
+// }
+// while (i < 2);
+
 // appData.moneyPerDay = appData.budget / 30;
 // alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
@@ -97,6 +107,7 @@ if (appData.moneyPerDay < 100) {
 // } else {
 //     console.log("Произошла ошибка");
 // };
+
 // Ответы на вопросы
 // 1)в JS существует 7 типов данных
 // 2)При помощи console.log() в скобках 
